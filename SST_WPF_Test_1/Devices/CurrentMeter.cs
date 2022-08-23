a096196579d3a5fd3aa5ff17525a5b6d25671310
maksimmer348 <maksimmer348@gmail.com>
2022-08-23 16:42:53 +0300

    доделан конфигуратор випа на данный момент, работа над алгоритмом

---
+++ SST_WPF_Test_1/Devices/CurrentMeter.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SST_WPF_Test_1;
+
+public class  CurrentMeter : BaseDevice
+{
+    public CurrentMeter(string name) : base(name)
+    {
+        IsDeviceType = "Измеритель тока";
+    }
+}
+
+internal enum MeterMode
+{
+    VoltageOut1,
+    VoltageOut2
+}
